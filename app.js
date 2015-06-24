e266b59170da68da0c2b8e13c3099f950c515129
Juan Carlos Espinosa Ceniceros <juank_kenny@hotmail.com>
2015-06-23 23:03:23 -0500

    Configuracion IP, correr con pm2, reverse proxy con nginx

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ server.on('request', function(request, response) {});
 server.on('close', function() {});
 
 // Server listen port
-server.listen(8000, function() {
-  console.log('Listening on port %d', server.address().port);
+server.listen(8000, '127.0.0.1', function() {
+  console.log('Listening on %j', server.address());
 });
