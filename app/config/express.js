daf8506439e10a8a04f2c9d731b0249e1bbeb95b
Juan Carlos Espinosa Ceniceros <juank_kenny@hotmail.com>
2015-07-06 22:06:39 -0500

    Templates ejs, prueba de rutas

--- app/config/express.js
+++ app/config/express.js
@@ -3,11 +3,12 @@ module.exports = function(app, express) {
   // This enables things like javascript and stylesheets to be loaded as expected.
   // You would normally use something like nginx for this normally,
   // but this makes for a simpler demo app to just let express do it.
-  app.use("/", express.static(__dirname + '/../../public'));
+  app.use(express.static(__dirname + '/../../public'));
 
   // Set the view directory, this enables us to use the .render method inside routes
   app.set('views', __dirname + '/../views'); 
 
   // Set the template engine to ejs
   app.set('view engine', 'ejs');
+  app.engine('.ejs', require('ejs').__express);
 }
--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // Load Express Configuration
 require('./config/express')(app, express);
 
 // Root route
-app.get('/', function(req, res) {
-  res.sendFile('index.html', {root: app.settings.views});
+app.get('/chat', function(req, res) {
+  res.render('index');
+});
+
+// Root route
+app.get('/about', function(req, res) {
+  res.sendFile('about.html', {root: path.join(__dirname, '../public')});
 });
 
 module.exports = app;
